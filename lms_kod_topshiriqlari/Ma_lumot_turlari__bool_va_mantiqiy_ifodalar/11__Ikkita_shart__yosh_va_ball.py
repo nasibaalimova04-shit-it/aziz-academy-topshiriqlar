b3912e1a16e49e3eb7bcc2591c364b8db93e5469
nasibaalimova04-shit-it <nasibaalimova04@gmail.com>
2026-05-29 17:10:28 +0500

    Add: 11) Ikkita shart: yosh va ball [nasibaalimova04-shit-it]

---
+++ lms_kod_topshiriqlari/Ma_lumot_turlari__bool_va_mantiqiy_ifodalar/11__Ikkita_shart__yosh_va_ball.py
@@ -0,0 +1,9 @@
+yosh = int(input())
+ball = int(input())
+natija = (yosh <= 25) and (ball >= 90) 
+print(natija)                         
+                          
+                          
+                          
+                          
+                        
