5b98dfc36d7c1f83352f89629303eafe7e586ab3
nasibaalimova04-shit-it <nasibaalimova04@gmail.com>
2026-05-29 17:26:06 +0500

    Add: 8) 0 kiritsang to‘xtaydi, manfiylarni skip (continue) [nasibaalimova04-shit-it]

---
+++ lms_kod_topshiriqlari/while_bilan_break_va_continue/8__0_kiritsang_to_xtaydi__manfiylarni_skip__contin.py
@@ -0,0 +1,28 @@
+y = 0 
+while True:
+    s = int(input()) 
+    if s == 0:
+        break
+    if s < 0:
+        continue
+    y += s   
+print(y)    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
