62a7b6e6ca5191a9aa6df298ca671d2abc8a458f
nasibaalimova04-shit-it <nasibaalimova04@gmail.com>
2026-06-12 17:42:29 +0500

    Add: 5) Maksimum va minimum farqi [nasibaalimova04-shit-it]

---
+++ lms_kod_topshiriqlari/Loop___hisoblash__maksimum__minimum__yig_indi__o_r/5__Maksimum_va_minimum_farqi.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int,input().split()))
+mx = a[0]
+mn = a[0]
+for i in range(1, n):
+    if a[i] > mx:
+        mx = a[i]
+    if a[i] < mn:
+        mn = a[i]
+print(mx - mn)
+
+
