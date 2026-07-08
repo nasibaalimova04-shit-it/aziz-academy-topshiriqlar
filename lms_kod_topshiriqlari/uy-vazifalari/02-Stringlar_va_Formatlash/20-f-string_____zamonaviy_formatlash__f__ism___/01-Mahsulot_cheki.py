fb74bb435e673c480bda311bb6c395feedabee0b
nasibaalimova04-shit-it <nasibaalimova04@gmail.com>
2026-07-09 00:35:26 +0500

    Add: Mahsulot cheki [nasibaalimova04-shit-it]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Mahsulot_cheki.py
@@ -0,0 +1,9 @@
+mahsulot = input()
+narx = int(input())
+soni = int(input())
+jami = narx * soni 
+print(f"Mahsulot: {mahsulot}")
+print(f"Narxi: {narx} som")
+print(f"Soni: {soni}")
+print(f"Jami: {jami} som")
+
